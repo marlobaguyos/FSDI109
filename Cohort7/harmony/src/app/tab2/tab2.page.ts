be56f6244796cefaf01e8a55620388740aa057cc
Marlo <marlo.baguyos@outlook.com>
2020-04-14 17:23:25 -0400

    update

--- Cohort7/harmony/src/app/tab2/tab2.page.ts
+++ Cohort7/harmony/src/app/tab2/tab2.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Message } from '../models/message';
+import { ShareService } from '../services/share.service';
 
 @Component({
   selector: 'app-tab2',
@@ -9,10 +10,13 @@ import { Message } from '../models/message';
 export class Tab2Page {
 
   message: Message = new Message();
+
+  constructor(private share: ShareService)
+
   constructor() {}
 
   onPost() {
-    this.message.from = 'Marlo';
+    this.message.from = this.share.userName;
     this.message.createdOn = new Date();
     console.log('posting', this.message);
   }
