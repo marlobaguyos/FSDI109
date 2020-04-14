646c3a9a14529e40af6cfa64393803c22f77cbfd
Marlo <marlo.baguyos@outlook.com>
2020-04-14 19:33:35 -0400

    update

--- Cohort7/harmony/src/app/services/data.service.ts
+++ Cohort7/harmony/src/app/services/data.service.ts
@@ -1,21 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Message } from '../models/message';
 import { Observable } from 'rxjs';
+import { AngularFirestoreCollection } from 'angularfire2/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  allMessage: Observable<Message[]>;
+  allMessages: Observable<Message[]>;
+  messageCollection: AngularFirestoreCollection<Message>; //pipeline to firebase database
 
-  constructor() { }
+  constructor(private fb: AngularFirestore) {
+    this.messageCollection = fb.collection<Message>('post'); //initialize connection app -> firebase
+  }
 
   public saveMessage(message) {
-    this.allMessage.push(message);
+    this.allMessages.push(message);
   }
 
   public getAllMessages() {
-    return this.allMessage;
+    return this.allMessages;
   }
 }
