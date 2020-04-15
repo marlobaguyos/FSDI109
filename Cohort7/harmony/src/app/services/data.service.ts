f213d0aed639b1706d7eade4e06fe1c49a2f15d5
Marlo <marlo.baguyos@outlook.com>
2020-04-14 20:48:41 -0400

    update

--- Cohort7/harmony/src/app/services/data.service.ts
+++ Cohort7/harmony/src/app/services/data.service.ts
@@ -2,28 +2,47 @@ import { Injectable } from '@angular/core';
 import { Message } from '../models/message';
 import { Observable } from 'rxjs';
 import { AngularFirestoreCollection, AngularFirestore } from 'angularfire2/firestore';
+import { Friend } from '../models/friend';
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
   allMessages: Observable<Message[]>;
-  messageCollection: AngularFirestoreCollection<Message>; //pipeline to firebase database
+  messageCollection: AngularFirestoreCollection<Message>; // pipeline to firebase database
+
+  allFriends: Observable<Friend[]>;
+  friendCollection: AngularFirestoreCollection<Friend>; // pipeline to firebase db
 
   constructor(private fb: AngularFirestore) {
-    this.messageCollection = fb.collection<Message>('posts'); //initialize connection app -> firebase
+    this.messageCollection = fb.collection<Message>('posts'); // initialize connection app -> firebase
+    this.friendCollection = fb.collection<Friend>('friend'); // initialize connection
   }
 
   retrieveMessagesFromDB() {
     this.allMessages = this.messageCollection.valueChanges();
   }
 
+  retrieveFriendsFromDB() {
+    this.allFriends = this.friendCollection.valueChanges();
+  }
+
   public saveMessage(message) {
-    var plain = Object.assign({}, message);
+    const plain = Object.assign({}, message);
     this.messageCollection.add(plain);
   }
 
   public getAllMessages() {
     return this.allMessages;
+  }
+
+  public saveFriend(friend) {
+    const plain = Object.assign({}, friend);
+    this.friendCollection.add(plain);
+  }
+
+  public getAllFriends() {
+    this.retrieveFriendsFromDB();
+    return this.allFriends;
   }
 }
--- Cohort7/harmony/src/app/tab3/tab3.page.ts
+++ Cohort7/harmony/src/app/tab3/tab3.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Friend } from '../models/friend';
+import { SharedService } from '../services/shared.service';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-tab3',
@@ -10,9 +12,12 @@ export class Tab3Page {
 
   friend: Friend = new Friend();
 
-  constructor() {}
+  constructor(private shared: SharedService, private data: DataService) {}
 
-  onSave(){
+  onSave() {
+    this.friend.belongsTo = this.shared.userName;
     console.log(this.friend);
+
+    this.data.saveFriend(this.friend);
   }
 }
