c283ff1cc484dbc0a84cc97266180a9cadb91193
Marlo <marlo.baguyos@outlook.com>
2020-04-14 21:54:02 -0400

    update

--- Cohort7/harmony/src/app/tab2/tab2.page.ts
+++ Cohort7/harmony/src/app/tab2/tab2.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Message } from '../models/message';
 import { SharedService } from '../services/shared.service';
 import { DataService } from '../services/data.service';
+import { Friend } from '../models/friend';
 
 @Component({
   selector: 'app-tab2',
@@ -11,8 +12,21 @@ import { DataService } from '../services/data.service';
 export class Tab2Page {
 
   message: Message = new Message();
+  myFriends: Friend[] = [];
 
-  constructor( private shared: SharedService, private data: DataService) {}
+  constructor( private shared: SharedService, private data: DataService) {
+    this.data.getAllFriends().subscribe(list => {
+      // NOTE: the list contains all the friend in the DB, not only mine
+      // this.myFriends = list;
+      this.myFriends = [];
+      for(let i = 0; i < list.length; i++) {
+        var f = list[i];
+        if(f.belongsTo == this.shared.userName){
+          this.myFriends.push(f);
+        }
+      }
+    });
+  }
 
   onPost() {
     this.message.from = this.shared.userName;
